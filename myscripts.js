3a5f2fa7b608b664087b5023a67f395dc9997f60
Anjacodes <anja_schmidt7@posteo.de>
2021-11-22 14:35:05 -0600

    Create RemoveBook Function

--- myscripts.js
+++ myscripts.js
@@ -1,26 +1,48 @@
+// SELECTORS
+
 const collection = [];
+const addbtn = document.getElementById("add");
+const removeBtn = document.getElementById("remove");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+
+// OBJECT CONSTRUCTOR FUNCTION
 
 function Book(title,author) {
     this.title = title;
     this.author = author;
-    
   }
-  const book = new Book('Eagle', 'Talon TSi');
-  collection.push(book);
-console.log(collection);
-const addbtn = document.getElementById("add");
+
+//   const book = new Book('Eagle', 'Talon TSi');
+//   collection.push(book);
+// console.log(collection);
+
+// FUNCTON DECLARATIONS
 
 addbook = function() {
     const titleinput = document.getElementById("titleInput").value;
     const authorinput = document.getElementById("authorInput").value;
-    const book = new Book(titleinput, authorinput);
-    collection.push(book);
+    if (!titleinput == '' && !authorinput == '') {
+      const book = new Book(titleinput, authorinput)
+      collection.push(book);}
     console.log("clicked");
     console.log(collection);
+    title.innerText = titleinput;
+    author.innerText = authorinput;
 }
+
+removeBook = () => {
+  collection.filter((book, index, newColl) => { console.log(book[index].title);}
+  )};
+
+removeBook();
+
+// EVENT LISTENERS
+
 addbtn.addEventListener("click",addbook);
-const titleinput = document.getElementById("titleInput").value;
-const authorinput = document.getElementById("authorInput").value;
-const book2 = new Book(titleinput, authorinput);
-collection.push(book2);
-console.log(collection);
+removeBtn.addEventListener("click", removeBook);
+// const titleinput = document.getElementById("titleInput").value;
+// const authorinput = document.getElementById("authorInput").value;
+// const book2 = new Book(titleinput, authorinput);
+// collection.push(book2);
+// console.log(collection);
