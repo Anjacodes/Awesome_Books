e464f9dca2a8848e5e4b854f1ac9aeea0c51204b
Anjacodes <anja_schmidt7@posteo.de>
2021-11-23 15:41:39 -0600

    Fix Linter Errors

--- myscripts.js
+++ myscripts.js
@@ -1,91 +1,81 @@
 // SELECTORS
 
 const collection = [];
-const addbtn = document.getElementById("add");
-// title = document.getElementById("title");
+const addbtn = document.getElementById('add');
+// title = document.getElementById('title');
  var title;
-const author = document.getElementById("author");
-const bookContainer = document.getElementById("bookContainer");
-let btnValue;
+const author = document.getElementById('author');
+const bookContainer = document.getElementById('bookContainer');
+let storageObject ;
 
 // OBJECT CONSTRUCTOR FUNCTION
 
-document.addEventListener ('DOMContentLoad', () => {
-  if (localStorage === "") {
-window.localStorage.getItem('book');
-  }
+window.addEventListener ('load', () => {
+  storageObject = JSON.parse(localStorage.getItem('books'));
+  createBook(storageObject);
+  update();
 })
 
 function Book(title,author) {
-    this.title = title;
-    this.author = author;
-  }
+  this.title = title;
+  this.author = author;
+}
 
 // FUNCTON DECLARATIONS
 
-addbook = function() {
-    const titleinput = document.getElementById("titleInput").value;
-    const authorinput = document.getElementById("authorInput").value;
-    if (!titleinput == '' && !authorinput == '') {
-      const book = new Book(titleinput, authorinput);
-      collection.push(book);}
-      localStorage.setItem('books', JSON.stringify(collection));
-    // console.log(collection);
-    bookContainer.innerHTML = '';
-    createBook(collection);
-    update();
-}
-
-const booksHtmlTemplate = (obj) =>
-  `<div class="bookCont">
-  <label class="title">${obj.title}</label><br>
-  <label id="author">${obj.author}</label><br>
-  <button class='remove' type='button' value=''>Remove</button>
+const booksHtmlTemplate = (obj) => `<div class='bookCont'>
+  <label class='title'>${obj.title}</label><br>
+  <label id='author'>${obj.author}</label><br>
+  <button class='remove' type='button' onclick= value=''>Remove</button>
   <hr>
   </div>`;
 
 const createBook = (arr) => {
   arr.forEach((element) => {
-    // console.log(arr);
     const dinContent = document.createDocumentFragment();
     const newDiv = document.createElement('div');
-    // console.log(element);
-    // console.log(booksHtmlTemplate(element));
     newDiv.innerHTML = booksHtmlTemplate(element);
-    localStorage.setItem('book', JSON.stringify(element));
-    console.log(element.title, element.author);
     dinContent.appendChild(newDiv);
     bookContainer.appendChild(dinContent);
   });
 };
 
-// EVENT LISTENERS
-update = function() {
-  var elements = document.getElementsByClassName("remove");
-console.log(elements);
-title = document.getElementsByClassName("title");
-console.log(title);
-const bookCont = document.querySelectorAll('.bookCont');
-console.log(bookCont);
-
-for (let i = 0; i < elements.length; i++) {
-  elements[i].addEventListener('click', () => {
-      console.log("remove clicked");
-      console.log(title[i].innerHTML, bookCont[i]);
-      collection.splice(i, 1);
-      console.log(collection);
-      bookContainer.innerHTML = "";
-      createBook(collection);
+addbook = function () {
+  const titleinput = document.getElementById('titleInput').value;
+  const authorinput = document.getElementById('authorInput').value;
+  if (!titleinput == '' && !authorinput == '') {
+    const book = new Book(titleinput, authorinput);
+    collection.push(book);
+      localStorage.setItem('books', JSON.stringify(collection));
+    if (bookContainer.innerHTML !== '') {
+      bookContainer.innerHTML += booksHtmlTemplate(book);
       update();
-     });
-}
+    } else {
+      bookContainer.innerHTML = '';
+      createBook(storageObject);
+      update();
+    }
+  }
 
-}
+};
 
-addbtn.addEventListener("click",addbook);
-// // const removeBtn = document.querySelectorAll('.remove');
-// removeBtn.forEach((button) => {
-//   button.addEventListener('click', (e) => {
-//    alert(e.target);
-//   });
-// });
+// EVENT LISTENERS
+update = () => {
+  const elements = document.getElementsByClassName('remove');
+  title = document.getElementsByClassName('title');
+  const bookCont = document.querySelectorAll('.bookCont');
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', () => {
+      // console.log('remove clicked');
+      if (bookCont[i].parentNode) {
+        bookCont[i].parentNode.removeChild(bookCont[i]);
+      }
+      collection.splice(i, 1);
+      localStorage.setItem('books', JSON.stringify(collection));
+    });
+  }
+
+};
+
+addbtn.addEventListener('click', addbook);
