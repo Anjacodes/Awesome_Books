990661468bbc5b1b13b804726d023b96b14b6897
Anjacodes <anja_schmidt7@posteo.de>
2021-11-23 18:27:11 -0600

    Fix suggestions from code review

--- myscripts.js
+++ myscripts.js
@@ -26,15 +26,15 @@ const createBook = (arr) => {
 const update = () => {
   const elements = document.getElementsByClassName('remove');
   const bookCont = document.querySelectorAll('.bookCont');
-
+  storageObject = JSON.parse(localStorage.getItem('books'));
   for (let i = 0; i < elements.length; i += 1) {
     elements[i].addEventListener('click', () => {
       // console.log('remove clicked');
       if (bookCont[i].parentNode) {
         bookCont[i].parentNode.removeChild(bookCont[i]);
+        storageObject.splice(i, 1);
+        localStorage.setItem('books', JSON.stringify(storageObject));
       }
-      collection.splice(i, 1);
-      localStorage.setItem('books', JSON.stringify(collection));
     });
   }
 };
@@ -43,8 +43,10 @@ const update = () => {
 
 window.addEventListener('load', () => {
   storageObject = JSON.parse(localStorage.getItem('books'));
-  createBook(storageObject);
+  if (storageObject !== '') {
+    createBook(storageObject);
   update();
+  }
 });
 
 function Book(title, author) {
@@ -57,18 +59,14 @@ function Book(title, author) {
 const addbook = function () {
   const titleinput = document.getElementById('titleInput').value;
   const authorinput = document.getElementById('authorInput').value;
+  storageObject = JSON.parse(localStorage.getItem('books'));
   if (titleinput !== '' && authorinput !== '') {
     const book = new Book(titleinput, authorinput);
-    collection.push(book);
-    localStorage.setItem('books', JSON.stringify(collection));
-    if (bookContainer.innerHTML !== '') {
-      bookContainer.innerHTML += booksHtmlTemplate(book);
-      update();
-    } else {
-      bookContainer.innerHTML = '';
-      createBook(storageObject);
-      update();
-    }
+    storageObject.push(book);
+    console.log(storageObject);
+    localStorage.setItem('books', JSON.stringify(storageObject));
+    bookContainer.innerHTML += booksHtmlTemplate(book);
+    update();
   }
 };
 
