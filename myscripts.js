8dd24e3e13f53f131bd0cd6ab711510154ad9f03
Anjacodes <anja_schmidt7@posteo.de>
2021-11-25 09:51:23 -0600

    Add header and date

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,3 @@
-// const addbook = function () {
-//   const titleinput = document.getElementById('titleInput').value;
-//   const authorinput = document.getElementById('authorInput').value;
-//   bookmgr.storageObject = JSON.parse(localStorage.getItem('books'));
-//   if (titleinput !== '' && authorinput !== '') {
-//     const book = new Book_(titleinput, authorinput);
-//     if (bookmgr.storageObject === '') {
-//       bookmgr.storageObject.push(book);
-//       localStorage.setItem('books', JSON.stringify(bookmgr.storageObject));
-//       DocAccesor.bookContainer.innerHTML += booksHtmlTemplate(book);
-//     } else {
-//       bookmgr.addBooks(book);
-//       localStorage.setItem('books', JSON.stringify(bookmgr.collection));
-//       DocAccesor.bookContainer.innerHTML += booksHtmlTemplate(book);
-//     }
-//     update();
-//   }
-// };
-
 class BookManager {
   collection = [];
 
@@ -113,57 +94,10 @@ class BookManager {
 }
 
 let bookmgr = new BookManager();
-// SELECTORS
+let DateTime = luxon.DateTime;
+const now = DateTime.now();
+let displayDate= now.toLocaleString(DateTime.DATETIME_MED);
 
-// const addbtn = document.getElementById('add');
-// title = document.getElementById('title');
-// const bookContainer = document.getElementById('bookContainer');
-// new let storageObject = '';
+const date = document.querySelector('.date');
 
-// new const booksHtmlTemplate = (obj) => `<div class='bookCont'>
-//   <label class='title'>${obj.title}</label><br>
-//   <label id='author'>${obj.author}</label><br>
-//   <button class='remove' type='button' onclick= value=''>Remove</button>
-//   <hr>
-//   </div>`;
-
-// new const createBook = (arr) => {
-//   arr.forEach((element) => {
-//     const dinContent = document.createDocumentFragment();
-//     const newDiv = document.createElement('div');
-//     newDiv.innerHTML = DocAccesor.booksHtmlTemplate(element);
-//     dinContent.appendChild(newDiv);
-//     DocAccesor.bookContainer.appendChild(dinContent);
-//   });
-// };
-
-// new2 const update = () => {
-//   const elements = document.getElementsByClassName('remove');
-//   const bookCont = document.querySelectorAll('.bookCont');
-//    bookmgr.collection = JSON.parse(localStorage.getItem('books'));
-//   Array.from(elements).forEach((btn, index) => {
-//     btn.addEventListener('click', () => {
-//       if (bookCont[index].parentNode) {
-//         bookCont[index].parentNode.removeChild(bookCont[index]);
-//         bookmgr.collection.splice(index, 1);
-//         localStorage.setItem('books', JSON.stringify(bookmgr.collection));
-//       }
-//     });
-//   });
-// };
-
-// OBJECT CONSTRUCTOR FUNCTION
-
-// new3 window.addEventListener('load', () => {
-//   bookmgr.storageObject = JSON.parse(localStorage.getItem('books'));
-//   if (bookmgr.storageObject) {
-//     bookmgr.createBook(bookmgr.storageObject);
-//     bookmgr.update();
-//   } else {
-//     DocAccesor.bookContainer.innerHTML = '';
-//   }
-// });
-
-// FUNCTON DECLARATIONS
-
-// EVENT LISTENERS
+date.innerHTML = displayDate;
